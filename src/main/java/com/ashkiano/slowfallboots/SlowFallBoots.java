8d43f947e979d2cd6c0fa9acc959e4e22b6c8152
Josef Vyskoèil <josef.vyskocil@cleverlance.com>
2023-09-25 22:37:19 +0200

    version 1.2

--- src/main/java/com/ashkiano/slowfallboots/SlowFallBoots.java
+++ src/main/java/com/ashkiano/slowfallboots/SlowFallBoots.java
@@ -10,5 +10,6 @@ public final class SlowFallBoots extends JavaPlugin {
         getCommand("slowfallboots").setExecutor(new SlowFallBootsCommand());
         getServer().getPluginManager().registerEvents(new SlowFallBootsListener(this), this);
         Metrics metrics = new Metrics(this,19516);
+        this.getLogger().info("Thank you for using the SlowFallBoots plugin! If you enjoy using this plugin, please consider making a donation to support the development. You can donate at: https://donate.ashkiano.com");
     }
 }
